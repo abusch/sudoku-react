9b3b13d1f52a421ce268b234516fab04749078c5
Antoine Busch <antoine.busch@gmail.com>
2021-09-15 15:23:41 +1000

    Switch to function components + hooks

--- src/App.js
+++ src/App.js
@@ -1,32 +1,29 @@
 import './App.css';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
-class Cell extends React.Component {
-
-  render() {
-    let value = (this.props.value.value === 0)?" ":(this.props.value.value);
-    console.log("rendering cell...");
-    return <div className={"cell " + this.props.selection}>
+export const Cell = (props) => {
+    let value = (props.value.value === 0)?" ":(props.value.value);
+    let locked = (props.value.locked)?"locked ":" ";
+    return <div className={"cell " + locked + props.selection}>
       {value}
     </div>;
-  }
 }
 
-class Board extends React.Component {
-  renderBlock(blockX, blockY) {
+export const Board = (props) => {
+  const renderBlock = (blockX, blockY) => {
     var cells = Array(9);
-    var data = this.props.cellData;
+    var data = props.cellData;
 
     for(var j = 0; j < 3; j++) {
       let y = blockY * 3 + j;
       for(var i = 0; i < 3; i++) {
         let x = blockX * 3 + i;
         var selection;
-        if (x === this.props.selectedColumn && y === this.props.selectedRow) {
+        if (x === props.selectedColumn && y === props.selectedRow) {
           selection = "selectedCell";
-        } else if (x === this.props.selectedColumn) {
+        } else if (x === props.selectedColumn) {
           selection = "selectedColumn";
-        } else if (y === this.props.selectedRow) {
+        } else if (y === props.selectedRow) {
           selection = "selectedRow";
         } else {
           selection = "";
@@ -35,30 +32,25 @@ class Board extends React.Component {
       }
     }
     return <div className="block">
-    {cells}
+      {cells}
     </div>
-  }
+  };
 
-  render() {
-    console.log("Rendering board...");
     return <div className="board">
-      {this.renderBlock(0,0)}
-      {this.renderBlock(1,0)}
-      {this.renderBlock(2,0)}
-      {this.renderBlock(0,1)}
-      {this.renderBlock(1,1)}
-      {this.renderBlock(2,1)}
-      {this.renderBlock(0,2)}
-      {this.renderBlock(1,2)}
-      {this.renderBlock(2,2)}
+      {renderBlock(0,0)}
+      {renderBlock(1,0)}
+      {renderBlock(2,0)}
+      {renderBlock(0,1)}
+      {renderBlock(1,1)}
+      {renderBlock(2,1)}
+      {renderBlock(0,2)}
+      {renderBlock(1,2)}
+      {renderBlock(2,2)}
     </div>;
-  }
 }
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    let data = [
+const App = () => {
+    const data = [
       0, 0, 1, 2, 0, 3, 4, 0, 0,
       0, 0, 0, 6, 0, 7, 0, 0, 0,
       5, 0, 0, 0, 0, 0, 0, 0, 3,
@@ -68,22 +60,19 @@ class App extends React.Component {
       1, 0, 0, 0, 0, 0, 0, 0, 8,
       0, 0, 0, 8, 0, 5, 0, 0, 0,
       0, 0, 6, 4, 0, 2, 5, 0, 0];
-    this.state = {
+    const [boardData, setBoardData] = useState({
       data: data.map(v => {
         return {value : v, locked: (v !== 0)};
       }),
       selectedRow: 1,
       selectedColumn: 1
-    };
+    });
 
-    this.handleKeyDown = this.handleKeyDown.bind(this);
-  }
-
-  handleKeyDown(event) {
-    var x = this.state.selectedColumn;
-    var y = this.state.selectedRow;
+  const handleKeyDown = (event) => {
+    var x = boardData.selectedColumn;
+    var y = boardData.selectedRow;
     console.log("In handleKeyDown: event = " + event.key);
-    console.log("state = " + this.state);
+    console.log("state = " + boardData);
     console.log("current selected: " + x + ", " + y);
 
     if (event.key === 'j' || event.key === 'ArrowDown') {
@@ -111,11 +100,11 @@ class App extends React.Component {
         x = x + 1;
       }
     }
-    this.setState({selectedColumn: x, selectedRow: y});
+    setBoardData({selectedColumn: x, selectedRow: y});
 
     const digits = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
     if (digits.includes(event.key)) {
-      this.setState((prevState) => {
+      setBoardData((prevState) => {
         var data = prevState.data.slice();
         var currentCell = data[y * 9 + x];
         if (!currentCell.locked) {
@@ -128,28 +117,24 @@ class App extends React.Component {
     console.log("After update selected: " + x + ", " + y);
   };
 
-  componentDidMount() {
-    console.log("Adding keyboard listener");
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [handleKeyDown]);
 
-  componentWillUnmount() {
-    console.log("Removing keyboard listener");
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
 
-  render() {
     return (
-      <div className="content" onKeyDown={this.handleKeyDown}>
+      <div className="content">
         <h1>SUDOKU</h1>
         <Board
-          cellData={this.state.data}
-          selectedRow={this.state.selectedRow}
-          selectedColumn={this.state.selectedColumn} />
+          cellData={boardData.data}
+          selectedRow={boardData.selectedRow}
+          selectedColumn={boardData.selectedColumn} />
         <footer>&copy; Copyright 2021, Antoine Busch</footer>
       </div>
     );
-  }
 }
 
 export default App;
