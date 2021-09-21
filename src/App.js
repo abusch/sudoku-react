4f25174d485f3a60d73668d21f2e344aebd82a2a
Antoine Busch <antoine.busch@gmail.com>
2021-09-21 13:02:53 +1000

    Small clean-ups + support Backspace

--- src/App.js
+++ src/App.js
@@ -9,35 +9,46 @@ export const Cell = (props) => {
   </div>;
 }
 
+const Block = (props) => {
+  const blockX = props.x;
+  const blockY = props.y;
+
+  const cells = [];
+  const data = props.cellData;
+  const { row, column } = props.selection;
+  const selectedBlockX = Math.floor(column / 3);
+  const selectedBlockY = Math.floor(row / 3);
+
+  for (let j = 0; j < 3; j++) {
+    const y = blockY * 3 + j;
+    for (let i = 0; i < 3; i++) {
+      const x = blockX * 3 + i;
+      let selection;
+      if (x === column && y === row) {
+        selection = "selectedCell";
+      } else if (x === column) {
+        selection = "selectedColumn";
+      } else if (y === row) {
+        selection = "selectedRow";
+      } else if (blockX === selectedBlockX && blockY === selectedBlockY) {
+        selection = "selectedBlock";
+      } else {
+        selection = "";
+      }
+      // Generate an appropriate key to make sure a cell is only rerendered when needed:
+      const key = x + "," + y + "," + selection;
+      cells.push(<Cell key={key} value={data[y * 9 + x]} selection={selection} />);
+    }
+  }
+
+  return <div className="block">
+    {cells}
+  </div>
+}
+
 export const Board = (props) => {
   const renderBlock = (blockX, blockY) => {
-    const cells = Array(9);
-    const data = props.cellData;
-    const selectedBlockX = Math.floor(props.selectedColumn / 3);
-    const selectedBlockY = Math.floor(props.selectedRow / 3);
-
-    for (let j = 0; j < 3; j++) {
-      const y = blockY * 3 + j;
-      for (let i = 0; i < 3; i++) {
-        const x = blockX * 3 + i;
-        let selection;
-        if (x === props.selectedColumn && y === props.selectedRow) {
-          selection = "selectedCell";
-        } else if (x === props.selectedColumn) {
-          selection = "selectedColumn";
-        } else if (y === props.selectedRow) {
-          selection = "selectedRow";
-        } else if (blockX === selectedBlockX && blockY === selectedBlockY) {
-          selection = "selectedBlock";
-        } else {
-          selection = "";
-        }
-        cells.push(<Cell key={x + "," + y + "," + selection} value={data[y * 9 + x]} selection={selection} />);
-      }
-    }
-    return <div className="block">
-      {cells}
-    </div>
+    return <Block x={blockX} y={blockY} cellData={props.cellData} selection={props.selection} />;
   };
 
   return <div className="board">
@@ -74,6 +85,7 @@ const App = () => {
 
   useEffect(() => {
     const handleKeyDown = (event) => {
+      const digits = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
       let x = selection.column;
       let y = selection.row;
       console.log("In handleKeyDown: event = " + event.key);
@@ -81,34 +93,18 @@ const App = () => {
       console.log("current selected: " + x + ", " + y);
 
       if (event.key === 'j' || event.key === 'ArrowDown') {
-        if (y === 8) {
-          y = 0;
-        } else {
-          y = y + 1;
-        }
+        y = (y === 8) ? 0 : y + 1;
+        setSelection({ column: x, row: y });
       } else if (event.key === 'k' || event.key === 'ArrowUp') {
-        if (y === 0) {
-          y = 8;
-        } else {
-          y = y - 1;
-        }
+        y = (y === 0) ? 8 : y - 1;
+        setSelection({ column: x, row: y });
       } else if (event.key === 'h' || event.key === 'ArrowLeft') {
-        if (x === 0) {
-          x = 8;
-        } else {
-          x = x - 1;
-        }
+        x = (x === 0) ? 8 : x - 1;
+        setSelection({ column: x, row: y });
       } else if (event.key === 'l' || event.key === 'ArrowRight') {
-        if (x === 8) {
-          x = 0;
-        } else {
-          x = x + 1;
-        }
-      }
-      setSelection({ column: x, row: y });
-
-      const digits = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
-      if (digits.includes(event.key)) {
+        x = (x === 8) ? 0 : x + 1;
+        setSelection({ column: x, row: y });
+      } else if (digits.includes(event.key)) {
         setBoardData((prevState) => {
           const data = prevState.slice();
           const currentCell = data[y * 9 + x];
@@ -117,9 +113,17 @@ const App = () => {
           }
           return data;
         })
-
+      } else if (event.key === 'Backspace') {
+        setBoardData((prevState) => {
+          const data = prevState.slice();
+          const currentCell = data[y * 9 + x];
+          if (!currentCell.locked) {
+            currentCell.value = 0;
+          }
+          return data;
+        })
       }
-      console.log("After update selected: " + x + ", " + y);
+      // console.log("After update selected: " + x + ", " + y);
     };
 
     window.addEventListener('keydown', handleKeyDown);
@@ -134,8 +138,8 @@ const App = () => {
       <h1>SUDOKU</h1>
       <Board
         cellData={boardData}
-        selectedRow={selection.row}
-        selectedColumn={selection.column} />
+        selection={selection}
+      />
       <footer>&copy; Copyright 2021, Antoine Busch</footer>
     </div>
   );
